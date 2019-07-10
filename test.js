afa20b5d4e3523cd06b89088808571cae68f3ef3
mkatenolan <mkatenolan@gmail.com>
2019-07-10 12:33:27 +0100

    Add first test - function returns an array

--- test.js
+++ test.js
@@ -5,3 +5,10 @@ test('Example test', function(t) {
   t.pass();
   t.end();
 });
+
+test('Testing that addTodo returns an array ', function(t) {
+  const actual = typeof logic.addTodo();
+  const expected = typeof [];
+  t.equal(actual, expected, 'addTodo should return an array');
+  t.end();
+});
